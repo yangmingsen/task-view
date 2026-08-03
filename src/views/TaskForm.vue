04875044f17a925ce008e7a7e746dd6a4743e74d
yangmingsen <yangmingesn16@outlook.com>
2026-08-03 20:41:12 +0800

    新建页面截止时间默认当前系统日期

--- src/views/TaskForm.vue
+++ src/views/TaskForm.vue
@@ -25,7 +25,7 @@ const form = ref({
   priority: 3,
   status: 'wait',
   assignedTo: '',
-  deadline: '',
+  deadline: new Date().toISOString().substring(0, 10),
   desc: '',
   project: '',
   module: '',
